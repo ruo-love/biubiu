0f9d9c86665139cb3077dff1cd74282f750cb7f9
zero <zl18180276692@outlook.com>
2023-11-16 11:13:33 +0800

    update

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ const updateRepo = () => {
   try {
     // 进入仓库目录
     process.chdir(repoPath);
-
     // 执行git pull命令
     execSync("git pull origin master");
+    console.log("代码拉取成功！");
     //更新package.json 中的updateTime 字段
     const packageJsonPath = path.resolve(__dirname, "./package.json");
     const packageJson = require(packageJsonPath);
     packageJson.updateTime = new Date().toLocaleString();
     // 写入package.json
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    console.log("updateTime 更新成功！");
     // push 到远程仓库
     execSync("git add .");
     execSync('git commit -m "update"');
     execSync("git push origin master");
-    console.log("仓库更新成功！");
+    console.log("远程仓库更新成功！");
   } catch (error) {
     console.error("仓库更新失败：", error.message);
   }
