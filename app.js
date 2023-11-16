7fca8c140d445528d044e578fbfda17452ae3768
zero <zl18180276692@outlook.com>
2023-11-16 11:03:13 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 // 设置你的仓库路径
-const repoPath = './';
+const repoPath = "./";
 
 // 更新仓库
 const updateRepo = () => {
@@ -11,11 +11,20 @@ const updateRepo = () => {
     process.chdir(repoPath);
 
     // 执行git pull命令
-    execSync('git pull origin main');
-
-    console.log('仓库更新成功！');
+    execSync("git pull origin master");
+    //更新package.json 中的updateTime 字段
+    const packageJsonPath = path.resolve(__dirname, "./package.json");
+    const packageJson = require(packageJsonPath);
+    packageJson.updateTime = new Date().toLocaleString();
+    // 写入package.json
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    // push 到远程仓库
+    execSync("git add .");
+    execSync('git commit -m "update"');
+    execSync("git push origin master");
+    console.log("仓库更新成功！");
   } catch (error) {
-    console.error('仓库更新失败：', error.message);
+    console.error("仓库更新失败：", error.message);
   }
 };
 
