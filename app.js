4cf0ede8f5eb0c18d8afe901cece8a8c71b990c0
zero <zl18180276692@outlook.com>
2024-03-08 11:08:33 +0800

    first commit

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const path = require("path");
 const fs = require("fs");
 // 设置你的仓库路径
 const repoPath = "./";
-const time = "2024 1/9 17.52";
+const time = "2024 1/8 17.52";
 // 更新仓库
 const updateRepo = () => {
-  try { 
-
+  try {
     // 进入仓库目录
     process.chdir(repoPath);
     // 执行git pull命令
     execSync("git pull origin master");
-    console.log("代码拉取成功！");                                                                                                                                          
+    console.log("代码拉取成功！");
     //更新package.json 中的updateTime 字段
     const packageJsonPath = path.resolve(__dirname, "./package.json");
     const packageJson = require(packageJsonPath);
